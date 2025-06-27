9e4ba17d32d6cc86c8cd7f034210199f3ee0bccf
Jarek Lin <s410673074@gmail.com>
2025-06-27 14:21:49 +0800

    feat: update interface

--- sdk/src/_generated/your-stable/index.ts
+++ sdk/src/_generated/your-stable/index.ts
@@ -1,1 +1,10 @@
-export const PACKAGE_ID = "0x5dc5ef7bcbcf4031b7012e58b6fa4f7fa4643e26ba25f54365da2ad73e845f15"; export const PUBLISHED_AT = "0x5dc5ef7bcbcf4031b7012e58b6fa4f7fa4643e26ba25f54365da2ad73e845f15"; export const PKG_V1 = "0x5dc5ef7bcbcf4031b7012e58b6fa4f7fa4643e26ba25f54365da2ad73e845f15";
+export const PACKAGE_ID =
+  "0x5dc5ef7bcbcf4031b7012e58b6fa4f7fa4643e26ba25f54365da2ad73e845f15";
+export let PUBLISHED_AT =
+  "0x5dc5ef7bcbcf4031b7012e58b6fa4f7fa4643e26ba25f54365da2ad73e845f15";
+export const PKG_V1 =
+  "0x5dc5ef7bcbcf4031b7012e58b6fa4f7fa4643e26ba25f54365da2ad73e845f15";
+
+export const setPublishedAt = (id: string) => {
+  PUBLISHED_AT = id;
+};
